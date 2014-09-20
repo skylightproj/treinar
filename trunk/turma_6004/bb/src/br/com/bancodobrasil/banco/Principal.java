798e00b9b8c3c241375c3c15dd6dfbddd85d5e60
gleidson.gmoura@gmail.com <gleidson.gmoura@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-20 01:09:31 +0000

    Share project "bb" into "https://treinar.googlecode.com/svn/trunk"
    Estou iniciando nosso projeto de estudos

---
+++ trunk/turma_6004/bb/src/br/com/bancodobrasil/banco/Principal.java
@@ -0,0 +1,11 @@
+package br.com.bancodobrasil.banco;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Bem vindos ao mundo da TI");
+	}
+	
+}
