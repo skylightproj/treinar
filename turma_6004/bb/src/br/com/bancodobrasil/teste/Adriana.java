3c22c661124ba5747018f34dc83c791db6843b81
aduelis@gmail.com <aduelis@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-20 01:21:02 +0000

    Criei a classe de boas vindas.

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/Adriana.java
@@ -0,0 +1,9 @@
+package br.com.bancodobrasil.teste;
+
+import javax.swing.JOptionPane;
+
+public class Adriana {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Olá Gleidson");
+	}
+}
