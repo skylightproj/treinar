1b6a77765db2458a21d07414db9b995aab2d5077
gleidson.gmoura@gmail.com <gleidson.gmoura@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-23 22:38:53 +0000

    criação do menu e chamada na classe responsável por inicializar o programa

--- trunk/turma_6004/bb/src/br/com/bancodobrasil/visao/TelaPrincipal.java
+++ trunk/turma_6004/bb/src/br/com/bancodobrasil/visao/TelaPrincipal.java
@@ -3,29 +3,37 @@ package br.com.bancodobrasil.visao;
 import javax.swing.JOptionPane;
 
 public class TelaPrincipal {
-
+			
 	public void init() {
 		
 		Integer opcao = -1;
 		
 		do {
-			String msg = "Digite:\n1-Depositar\n"
-					+ "2-Exibir Saldo"
+			String msg = "Digite:\n1-Criar Conta\n"
+					+ "2-Depositar\n"
+					+ "3-Exibir Saldo\n"
 					+ "0-Sair";
 			String opcaoStr = JOptionPane.showInputDialog(msg);
 			opcao = Integer.parseInt(opcaoStr);
 			switch (opcao) {
 			case 1:
-				//Alanderson
+				System.out.println("opcao 1");
 				break;
 			case 2:
+				System.out.println("opcao 2");
 				//Adriana
 				break;
+			case 3:
+				System.out.println("opcao 3");
+				//Erica
+				break;
 			case 0:
+				System.out.println("opcao sair 0");
 				//Erica
 				break;
 
 			default:
+				System.out.println("opcao errada");
 				break;
 			}
 			
