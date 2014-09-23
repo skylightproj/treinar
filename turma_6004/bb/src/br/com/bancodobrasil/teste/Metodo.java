6412d29efb497278a6efa8f0b577c460aecab9b0
gleidson.gmoura@gmail.com <gleidson.gmoura@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-23 23:29:01 +0000

    entendendo passagem de parametro e retorno de métodos

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/Metodo.java
@@ -0,0 +1,16 @@
+package br.com.bancodobrasil.teste;
+
+public class Metodo {
+
+	public String variavel;
+	
+	public void metodoUm() {
+		System.out.println("este é o retorno esperado por quem utilizar o metodoUm()");
+	}
+	public String metodoDois() {
+		//alguma coisa
+		String retorno = "depois do processamento eu fui retornada";
+		return retorno;
+	}
+	
+}
