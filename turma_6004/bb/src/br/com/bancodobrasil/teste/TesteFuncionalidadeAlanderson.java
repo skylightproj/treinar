fdf36d2e7a671cd7f4ebc6e35be6e50d6233a281
alandersonbenites@gmail.com <alandersonbenites@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-23 01:25:53 +0000

    Teste Funcionalidade Alanderson

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/TesteFuncionalidadeAlanderson.java
@@ -0,0 +1,23 @@
+package br.com.bancodobrasil.teste;
+
+import java.util.Scanner;
+
+import br.com.bancodobrasil.modelo.Conta;
+
+public class TesteFuncionalidadeAlanderson {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		
+		Scanner input = new Scanner(System.in);
+				
+		System.out.print("Informe o valor a ser depositado: ");
+		double valor = input.nextDouble();
+		
+		conta.depositar(valor);
+		conta.depositar(235.56);
+		
+		double saldoNovo = conta.recuperarSaldo();
+		System.out.println(saldoNovo);
+		
+	}
+}
