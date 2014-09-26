7f93559488cc0ef2344eb06da703e0ef7d8ffd66
aduelis@gmail.com <aduelis@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-26 01:01:40 +0000

    Implementação do método recuperar saldo na conta corrente.

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/TesteConstanteAdriana.java
@@ -0,0 +1,9 @@
+package br.com.bancodobrasil.teste;
+
+
+public class TesteConstanteAdriana {
+	public static void main(String[] args) {
+		//este valor não pode ser alterado porque este atributo é uma constante.
+		//BBUtil.nomeBanco = "Banco da Adriana";
+	}
+}
