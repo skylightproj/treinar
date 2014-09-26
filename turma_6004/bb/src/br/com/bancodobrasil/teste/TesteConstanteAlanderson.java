560e2a9b3c9cf6764e613ea64f32e708743934e0
alandersonbenites@gmail.com <alandersonbenites@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-26 23:12:14 +0000

    Implementado o método ArrayAlanderson (Método está incorreto), comitado apenas para comparação com a resposta do professor.

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/TesteConstanteAlanderson.java
@@ -0,0 +1,11 @@
+package br.com.bancodobrasil.teste;
+
+import br.com.bancodobrasil.util.BBUtil;
+
+public class TesteConstanteAlanderson {
+	public static void main(String[] args) {
+		//Este valor não pode ser alterado porque é uma constante
+		//BBUtil.nomeBanco = "Banco do Alanderson";
+	}
+
+}
