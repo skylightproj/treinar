3c8f222a60af73203c25799abe3bd943016833fc
gleidson.gmoura@gmail.com <gleidson.gmoura@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-27 18:21:13 +0000

    Funcionalidades da classe conta * depositar * sacar

--- turma_5685/bb_modelo/src/br/com/treinar/bb/Conta.java
+++ turma_5685/bb_modelo/src/br/com/treinar/bb/Conta.java
@@ -6,4 +6,18 @@ public class Conta {
 	public Double saldo;
 	public Cliente cliente;
 	
+	public Boolean sacar(Double valor) {
+		Boolean sacou = Boolean.FALSE;
+		if (saldo >= valor) {
+			saldo -= valor;
+			sacou = Boolean.TRUE;
+		}
+		return sacou;
+	}
+	
+	public void depositar(Double valor) {
+		saldo += valor;
+	}
+	
+	
 }
