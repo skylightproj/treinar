74cdd185bbe1b882e474508a4fb51732101f55f0
alandersonbenites@gmail.com <alandersonbenites@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-29 22:40:15 +0000

    Criado exemplo de impressão com Arrays

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/VetorImpressaoAlanderson.java
@@ -0,0 +1,17 @@
+package br.com.bancodobrasil.teste;
+
+public class VetorImpressaoAlanderson {
+	public static void main(String[] args) {
+		String [] nome = new String[5];  
+		nome[0] = "João";
+		nome[1] = "José";
+		nome[2] = "Maria";
+		nome[3] = "Joana";
+		nome[4] = "Djavan";
+		
+		for (int i = 0; i < nome.length; i++) {
+			System.out.println(nome[i]);
+		}
+	}
+
+}
