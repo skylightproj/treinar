97f21393fa6d33b5e690b90a68cf37d2f71d7961
ericacsgomes.eg@gmail.com <ericacsgomes.eg@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-29 22:38:58 +0000

    Criação de vetor

---
+++ trunk/turma_6004/bb/src/br/com/bancodobrasil/teste/VetorErica.java
@@ -0,0 +1,20 @@
+package br.com.bancodobrasil.teste;
+
+
+public class VetorErica {
+	public static void main(String[] args) {
+	
+		String[] nomes = new String[5];
+		nomes[0] = "Érica1";
+		nomes[1] = "Érica2";
+		nomes[2] = "Érica3";
+		nomes[3] = "Érica4";
+		nomes[4] = "Érica5";
+		
+		for (int i = 0; i < nomes.length; i++) {
+			System.out.println(nomes[i]);
+		}
+	}
+			
+
+}
