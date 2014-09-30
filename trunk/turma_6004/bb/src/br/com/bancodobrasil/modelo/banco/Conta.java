36346f11e751b54f872956cc10510ab3563b99ed
rafaview@gmail.com <rafaview@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-09-30 23:10:58 +0000

    Mudando a estrutura de dados vetor para para list

--- trunk/turma_6004/bb/src/br/com/bancodobrasil/modelo/banco/Conta.java
+++ trunk/turma_6004/bb/src/br/com/bancodobrasil/modelo/banco/Conta.java
@@ -30,7 +30,31 @@ public abstract class Conta {
 	 */
 	public abstract Double recuperarSaldo();
 			
-	public abstract Boolean sacar(Double valorSacado); 
+	public abstract Boolean sacar(Double valorSacado);
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((numeroConta == null) ? 0 : numeroConta.hashCode());
+		return result;
+	}
 	
-	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (numeroConta == null) {
+			if (other.numeroConta != null)
+				return false;
+		} else if (!numeroConta.equals(other.numeroConta))
+			return false;
+		return true;
+	} 
+
 }
