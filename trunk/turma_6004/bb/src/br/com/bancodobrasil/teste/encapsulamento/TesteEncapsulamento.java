8164ddef246cf6deef5724c89d8d5f780fd025d4
rafaview@gmail.com <rafaview@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-01 22:49:27 +0000

    criar um objeto da classe Pessoa, atibuir a este objeto nome e dataNascimento e apresentar estes atributos no console ou em uma janela

---
+++ trunk/turma_6004/bb/src/br/com/bancodobrasil/teste/encapsulamento/TesteEncapsulamento.java
@@ -0,0 +1,12 @@
+package br.com.bancodobrasil.teste.encapsulamento;
+
+import java.util.Date;
+
+public class TesteEncapsulamento {
+
+	public static void main(String[] args) {
+		Pessoa p = new Pessoa();
+		//p.dataNascimento = new Date();
+	}
+	
+}
