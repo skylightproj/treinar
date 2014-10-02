00fc2ee4895df0355770ac0c9cc24d90bde52e8a
rafaview@gmail.com <rafaview@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-02 19:20:47 +0000

    Exemplo simples de encapsulamento e tratamento de exceções.

---
+++ trunk/turma_6004/bb/src/br/com/bancodobrasil/teste/exercicio/exercicioRafaelExcecao/DistanciaInsuficienteException.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class DistanciaInsuficienteException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DistanciaInsuficienteException(String mensagem) {
+		super(mensagem);
+		
+	}
+
+
+}
