32ced7e61e4b36ee42c34efb3e8e2100a5e2e687
rafaview@gmail.com <rafaview@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-02 19:20:47 +0000

    Exemplo simples de encapsulamento e tratamento de exceções.

---
+++ turma_6004/bb/src/br/com/bancodobrasil/teste/exercicio/exercicioRafaelExcecao/Viagem.java
@@ -0,0 +1,81 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Viagem {
+
+	private int diasViajando;
+	private double distancia;
+	private boolean idaEVolta = false;
+	private double distanciaAdicional;
+	private double distanciaTotal;
+	private static Scanner entrada;
+
+
+
+	public double getDiasViajando() {
+		return diasViajando;
+	}
+
+	public void setDiasViajando(int diasViajando) {
+		this.diasViajando = diasViajando;
+	}
+
+	public double getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(double distancia) throws DistanciaInsuficienteException {
+		
+		if (distancia <= 0) {
+			throw new DistanciaInsuficienteException("Distância inválida");
+		} else {		
+			this.distancia = distancia;
+			System.out.println("A distância é: " + this.distancia + " km");
+		}
+	}
+
+	public boolean getIdaEVolta() {
+		return idaEVolta;
+	}
+
+	public void setIdaEVolta(boolean idaEVolta) {
+		
+		if (idaEVolta) {
+			this.distancia *= 2;
+		}
+	}
+
+	public double getDistanciaAdicional() {
+		return distanciaAdicional;
+	}
+
+	public void setDistanciaAdicional(double distanciaAdicional) {
+		this.distanciaAdicional = distanciaAdicional;
+	}
+
+	public double getDistanciaTotal() {
+		return distanciaTotal;
+	}
+
+	public void setDistanciaTotal() {
+		distanciaTotal = distancia + distanciaAdicional;
+	}
+	
+	public static void main(String[] args) {
+		
+		Viagem viagem = new Viagem();
+		entrada = new Scanner(System.in);
+		
+		try {
+			System.out.println("Informe a distância: ");
+			viagem.setDistancia(entrada.nextDouble());
+		} catch (DistanciaInsuficienteException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+
+
+}
