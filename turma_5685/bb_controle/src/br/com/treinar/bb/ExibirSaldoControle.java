efb86010bb9aeaf78d481f5769fae900f2bd7dc5
alano.david@gmail.com <alano.david@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-04 13:59:18 +0000

    Exibir Saldo

---
+++ turma_5685/bb_controle/src/br/com/treinar/bb/ExibirSaldoControle.java
@@ -0,0 +1,16 @@
+package br.com.treinar.bb;
+import java.util.Scanner;
+
+public class ExibirSaldoControle {
+
+	public static void main(String[] args) {
+        Scanner saldo = new Scanner(System.in);
+        
+        
+        System.out.println("Saldo: ");
+        
+        
+        System.out.printf("Seu Saldo é " + saldo + "\n");
+        
+    }
+}
