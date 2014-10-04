092f1e7a6eff105513619f9d44d70196f18b5c36
gleidson.gmoura <gleidson.gmoura@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-04 14:41:27 +0000

    selecionar tipo de conta

--- trunk/turma_5685/bb_controle/src/br/com/treinar/bb/BancoControle.java
+++ trunk/turma_5685/bb_controle/src/br/com/treinar/bb/BancoControle.java
@@ -2,16 +2,33 @@ package br.com.treinar.bb;
 
 import java.util.Date;
 
+import br.com.treinar.bb.banco.Conta;
 import br.com.treinar.bb.dado.BaseDados;
 
 public class BancoControle {
 
-	public void cadastrarConta(Long codigo, String nomeCliente, Date dataNascimento) {
-		BaseDados.getInstance().conta = new ContaCorrente();
+	private void cadastrarConta(Conta conta, Long codigo, String nomeCliente, Date dataNascimento) {
 		BaseDados.getInstance().conta.codigo = codigo;
 		BaseDados.getInstance().conta.cliente = new Cliente();
 		BaseDados.getInstance().conta.cliente.nome = nomeCliente;
 		BaseDados.getInstance().conta.cliente.dataNascimento = dataNascimento;
+	}
+
+	public void cadastrarContaCorrente(Long codigo, String nomeCliente,
+			Date dataNascimento, Double valorTarifa) {
+		BaseDados.getInstance().conta = new ContaCorrente();
+		Conta conta = BaseDados.getInstance().conta;
+		cadastrarConta(conta, codigo, nomeCliente, dataNascimento);
+		((ContaCorrente)conta).tarifa = valorTarifa;
+		
+	}
+
+	public void cadastrarContaPoupanca(Long codigo, String nomeCliente,
+			Date dataNascimento, Double taxaRendimento) {
+		BaseDados.getInstance().conta = new ContaPoupanca();
+		Conta conta = BaseDados.getInstance().conta;
+		cadastrarConta(conta, codigo, nomeCliente, dataNascimento);
+		((ContaPoupanca)conta).taxaRendimento = taxaRendimento;
 	}
 
 }
