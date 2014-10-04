1bc7fff3bd5342616d3428ea4326d0a686ba156f
gabriellima460@gmail.com <gabriellima460@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-04 18:29:21 +0000

    removendo teste

--- turma_5685/bb_controle/src/br/com/treinar/bb/DepositoControle.java
+++ turma_5685/bb_controle/src/br/com/treinar/bb/DepositoControle.java
@@ -6,8 +6,6 @@ public class DepositoControle {
 	
 	public void depositar(Double valor) {
 		BaseDados.getInstance().conta.depositar(valor);
-		BancoControle bc = new BancoControle();
-		System.out.println(bc.teste);
 	}
 	
 }
