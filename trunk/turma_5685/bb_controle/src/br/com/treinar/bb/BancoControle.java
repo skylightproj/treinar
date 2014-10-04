3d3aedc17c97c5eeb47345551f5c10f4c0e0f7ef
gleidson.gmoura <gleidson.gmoura@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-04 13:45:18 +0000

    evolucao do menu criacao dos casos de uso

--- trunk/turma_5685/bb_controle/src/br/com/treinar/bb/BancoControle.java
+++ trunk/turma_5685/bb_controle/src/br/com/treinar/bb/BancoControle.java
@@ -9,11 +9,11 @@ public class BancoControle {
 	public Conta conta;
 	
 	public void cadastrarConta(Long codigo, String nomeCliente, Date dataNascimento) {
-		conta = new Conta();
-		conta.codigo = codigo;
-		conta.cliente = new Cliente();
-		conta.cliente.nome = nomeCliente;
-		conta.cliente.dataNascimento = dataNascimento;
+//		conta = new Conta();
+//		conta.codigo = codigo;
+//		conta.cliente = new Cliente();
+//		conta.cliente.nome = nomeCliente;
+//		conta.cliente.dataNascimento = dataNascimento;
 	}
 	
 }
