64a605994463597d34618d7a7eaba0eb87855df8
raquel.mocinha@gmail.com <raquel.mocinha@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-25 12:36:18 +0000

    teste

--- trunk/turma_5685/aluno/Raquel/collection/src/colecoes/TestaAutomovel.java
+++ trunk/turma_5685/aluno/Raquel/collection/src/colecoes/TestaAutomovel.java
@@ -8,11 +8,20 @@ public class TestaAutomovel {
 		Automovel a = new Automovel();
 		a.setChassi(234556);
 		a.setTipo("Palio");
-		
+		Automovel a2 = new Automovel(); 
+		a2.setChassi(455666);
+		a2.setTipo("Siena");
+		Automovel a3 = new Automovel();
+		a3.setTipo("ka");
+		a3.setChassi(776654);
 		List<Automovel> lista = new ArrayList<>();
 		lista.add(a);
+		lista.add(a2);
+		lista.add(a3);
+		for (Automovel automovel : lista) {
+			System.out.println(automovel);
+		}
 		
-		System.out.println(lista);
 		
 		
 	}
