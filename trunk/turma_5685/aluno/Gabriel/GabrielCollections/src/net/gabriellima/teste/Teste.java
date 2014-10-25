402c9908c8eba7faaa449527b44e2e674e9c447d
gabriellima460@gmail.com <gabriellima460@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-10-25 13:06:31 +0000

    Add comparable implementatio.

--- trunk/turma_5685/aluno/Gabriel/GabrielCollections/src/net/gabriellima/teste/Teste.java
+++ trunk/turma_5685/aluno/Gabriel/GabrielCollections/src/net/gabriellima/teste/Teste.java
@@ -1,7 +1,9 @@
 package net.gabriellima.teste;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import net.gabriellima.objetos.Moto;
@@ -11,14 +13,38 @@ public class Teste {
 	public static void main(String[] args) {
 		List<Moto> motos = new ArrayList<>();
 		
-		Moto moto = new Moto();
-		moto.setModelo("CB 300R");
-		moto.setCor("Amarela");
-		moto.setDataFabricacao(new Date());
-		moto.setFabricante("Honda");
-		moto.setNumeroChassi("HSHDIE35453JSDK33");
+		Moto moto2 = new Moto();
+		moto2.setModelo("CB 300R");
+		moto2.setCor("Amarela");
+		Calendar c2 = Calendar.getInstance();
+		c2.set(Calendar.YEAR, 2014);
+		c2.set(Calendar.MONTH, 10);
+		c2.set(Calendar.DAY_OF_MONTH, 25);
+		moto2.setDataFabricacao(c2.getTime());
+		moto2.setFabricante("Honda");
+		moto2.setNumeroChassi("HSHDIE35453JSDK33");
 		
+		Moto moto = new Moto();
+		moto.setModelo("Fazer 250");
+		moto.setCor("Vermelha");
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.YEAR, 2010);
+		c.set(Calendar.MONTH, 1);
+		c.set(Calendar.DAY_OF_MONTH, 19);
+		moto.setDataFabricacao(c.getTime());
+		moto.setFabricante("Yamaha");
+		moto.setNumeroChassi("SDHTS72394NSJHBDSH38");		
+				
+		motos.add(moto2);
 		motos.add(moto);
+		
+		for (Moto motoCurrent : motos) {
+			System.out.println(motoCurrent);
+		}
+		
+		System.out.println("\n");
+		
+		Collections.sort(motos);
 		
 		for (Moto motoCurrent : motos) {
 			System.out.println(motoCurrent);
