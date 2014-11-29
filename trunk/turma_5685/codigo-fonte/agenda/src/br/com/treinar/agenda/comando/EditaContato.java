83982ab79b74d50dac49801dcfa549e2d253a5d7
gabriellima460@gmail.com <gabriellima460@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2014-11-29 15:51:24 +0000

    Caso de uso de Edição de Contato

--- trunk/turma_5685/codigo-fonte/agenda/src/br/com/treinar/agenda/comando/EditaContato.java
+++ trunk/turma_5685/codigo-fonte/agenda/src/br/com/treinar/agenda/comando/EditaContato.java
@@ -3,11 +3,26 @@ package br.com.treinar.agenda.comando;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.treinar.agenda.modelo.Contato;
+import br.com.treinar.agenda.util.DataBase;
+
 public class EditaContato implements Comando {
+	
+	private DataBase database;
+	
+	public EditaContato() {
+		database = DataBase.getInstance();
+	}
 
 	@Override
 	public String executar(HttpServletRequest req, HttpServletResponse resp) {
-		return "/pages/cadastroContato.html";
+		String email = req.getParameter("contato");
+		
+		Contato contato = database.getContatoById(email);
+		
+		req.setAttribute("contato", contato);
+		
+		return "/pages/editaContato.jsp";
 	}
 	
 }
