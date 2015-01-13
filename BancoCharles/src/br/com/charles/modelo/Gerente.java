0def7102c032c8c74418ff6cb274ce2ad6120a78
charleshrelv@gmail.com <charleshrelv@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-01-13 18:58:19 +0000

    Classe Gerente

---
+++ BancoCharles/src/br/com/charles/modelo/Gerente.java
@@ -0,0 +1,18 @@
+package br.com.charles.modelo;
+
+public class Gerente {
+	
+	String nome;
+	Double salario;
+	
+	public void aumentaSalario(){
+		
+		this.aumentaSalario();
+	}
+	
+	public void aumentaSalario(double taxa){
+		
+		this.salario += this.salario * taxa;
+	}
+
+}
