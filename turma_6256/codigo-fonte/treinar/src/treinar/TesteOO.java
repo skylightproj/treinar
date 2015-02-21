6ac73142ffeda4d724f72aae1ece2333bc568b46
danielaraujo.inf@gmail.com <danielaraujo.inf@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-02-21 18:14:18 +0000

    Imprimir o nome do aluno, professor e curso. Alterar variavel. Dispensar o recurso aluno.

--- turma_6256/codigo-fonte/treinar/src/treinar/TesteOO.java
+++ turma_6256/codigo-fonte/treinar/src/treinar/TesteOO.java
@@ -4,7 +4,7 @@ public class TesteOO {
 
 	public static void main(String[] args) {
 		Professor prof;
-		System.out.println("alguma coisa");
+		
 		prof = new Professor();
 		prof.nome = "Gleidson";
 		prof.idade = 34;
@@ -19,8 +19,24 @@ public class TesteOO {
 		curso.nome = "Java Básico";
 		curso.horasAula = 32;
 		curso.professor = prof;
+		curso.aluno = aluno;
 		
 		//TODO - imprimir o nome do professor e do aluno
+		
+		System.out.println("Curso: " + curso.nome);
+		System.out.println("Professor: " + curso.professor.nome);
+		System.out.println("Aluno: " + curso.aluno.nome);
+		
+		Aluno outro = new Aluno();
+		outro.nome = "Maria";
+		outro.idade = 23;
+		outro.matricula = 1234;
+		
+		aluno = null;
+		
+		curso.aluno = outro;
+		System.out.println("Aluno: " + curso.aluno.nome);
+		
 		
 	}
 	
