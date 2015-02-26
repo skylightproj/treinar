929b653e5a8e1eda96bceb95367376324427984f
priscila.srlacerda@gmail.com <priscila.srlacerda@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-02-26 00:07:31 +0000

    Exercício 1

---
+++ trunk/turma_6730/projeto/estudo/src/br/com/treinar/priscila/Exercício1.java
@@ -0,0 +1,9 @@
+package br.com.treinar.priscila;
+
+public class Exercício1 {
+	public static void main(String[] args) {
+		Nome meuNome = new Nome();
+		meuNome.nome = "Priscila";
+		System.out.println("Nome: " + meuNome.nome);
+	}
+}
