4cc2f20836111a43f4d86298a527d33e31af41a2
gabrielatleticanobh@gmail.com <gabrielatleticanobh@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-02-28 15:06:35 +0000

    Criei uma classe uma classe para imprimir meu nome

---
+++ turma_6256/codigo-fonte/estudo/src/gabriel/Exercicio1.java
@@ -0,0 +1,12 @@
+package gabriel;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio1 {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Gabriel");
+	
+	}
+
+}
