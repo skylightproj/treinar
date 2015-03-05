9aa6178d555f7badf1343cb545d0a5da436c3a13
humbertomonteiro@gmail.com <humbertomonteiro@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-03-05 23:47:09 +0000

    meu exericio enum

---
+++ trunk/turma_6730/projeto/estudo/src/br/com/treinar/humberto/ExercicioEnum.java
@@ -0,0 +1,27 @@
+package br.com.treinar.humberto;
+
+import java.util.Scanner;
+
+import br.com.treinar.tipodado.Mes;
+
+public class ExercicioEnum {
+	
+	public static void main(String[] args) {
+		System.out.print("Insira o número do mês: ");
+		Scanner sc = new Scanner(System.in);
+		int numMes = sc.nextInt();
+		
+		if (numMes > 0 && numMes <= 12) {
+			Mes[] meses = Mes.values();
+			System.out.println(meses[numMes - 1]);
+		
+		} else {
+			System.out.println("Número informado incompatível");
+		
+		}		
+			
+		sc.close();
+				
+	}
+
+}
