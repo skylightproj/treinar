21a4695f1771d011822d8764040268091ee207c9
danielaraujo.inf@gmail.com <danielaraujo.inf@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-03-14 19:29:09 +0000

    Implementação de regra para evitar que o vetor de contas assuma um estado inconsistente.

--- trunk/turma_6256/codigo-fonte/itau/src/br/com/treinar/itau/util/ItauUtil.java
+++ trunk/turma_6256/codigo-fonte/itau/src/br/com/treinar/itau/util/ItauUtil.java
@@ -12,7 +12,7 @@ public class ItauUtil extends Object {
 	private ItauUtil() {
 		super();
 		index = 0;
-		contas = new Conta[5];
+		contas = new Conta[3];
 	}
 	
 	static {
@@ -24,8 +24,12 @@ public class ItauUtil extends Object {
 	}
 	
 	public void adicionarConta(Conta c) {
+		if (index == contas.length) {
+			aumentarVetor();
+		}
 		contas[index++] = c;
 	}
+
 
 	public Conta recuperar(Integer numeroConta) {
 		Conta conta = null;
@@ -45,10 +49,41 @@ public class ItauUtil extends Object {
 	public void remover(Conta conta) {
 		for (int i = 0; i < contas.length; i++) {
 			if (contas[i] != null && contas[i].equals(conta)) {
+//				if (contas.length - 1 == i) {
+//					contas[i] = null;								
+//				} else if(contas.length - 2 == i) {					
+//					contas[contas.length - 2] = contas[contas.length - 1];								
+//					contas[contas.length - 1] = null;								
+//				} else {
+//					for (int j = i + 1; j < index; j++) {
+//						contas[i] = contas[j];					
+//					}					
+//				}
 				contas[i] = null;
+				realocar();
 				break;
 			}
 		}
 	}
+
+	private void realocar() {
+		Conta contasTemp[] = new Conta[contas.length];
+		int j = 0;
+		
+		for (int i = 0; i < contas.length; i++) {
+			if (contas[i] != null) {
+				contasTemp[j++] = contas[i];
+			}
+		}
+		contas = contasTemp;
+		index = j;
+	}
 	
+	private void aumentarVetor() {
+		Conta contasTemp[] = new Conta[contas.length * 2];
+		for (int i = 0; i < contas.length; i++) {
+			contasTemp[i] = contas[i];
+		}
+		contas = contasTemp;
+	}
 }
